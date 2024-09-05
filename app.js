d6c1e635f07899f13e8cc2257a5ac2033515e68d
gerardo vega <gerardoluisvega@gmail.com>
2024-09-05 16:40:00 -0300

    avances

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 import express from 'express'
+import moduleName, { PORT } from './src/settings/environments.js'
+const app = express()
+
+app.listen(PORT, ()=>{
+    console.log(`servidor corriendo en el puerto ${PORT}`);
+    });
